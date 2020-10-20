7cb06a5aa3f747fa3e1634d21c7a01eaa8361397
joel-chu <newbranltd@gmail.com>
2020-10-20 09:16:34 +0800

    setup the base test

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,1 +1,8 @@
 const test = require('ava')
+
+const { isWind, checkCobc } = require('../lib/check')
+
+
+test(`Check if this check method works correctly`, t => {
+  t.false(isWind(), 'It should not run on windows os')
+})
