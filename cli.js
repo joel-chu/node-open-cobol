2a0e3f2fd4cc1c1bfe472eb54fa5c3b861163b81
joel-chu <newbranltd@gmail.com>
2020-10-20 16:35:43 +0800

    copy over some of the examples

--- cli.js
+++ cli.js
@@ -1,2 +1,4 @@
 #! /usr/local/bin/node
-// The command line interface 
+// The command line interface
+const yargs = require('yargs')
+const { checkCobc } = require('./lib/check')
--- tests/main.test.js
+++ tests/main.test.js
@@ -1,8 +1,19 @@
 const test = require('ava')
 
 const { isWind, checkCobc } = require('../lib/check')
-
+const { getInput } = require('../lib/input')
 
 test(`Check if this check method works correctly`, t => {
   t.false(isWind(), 'It should not run on windows os')
+})
+
+test(`Testing the input with failed input`, async t => {
+  // expect to fail
+
+  const error = await t.throwsAsync(getInput('/path/to/bullshit'))
+  t.truthy(error)
+
+
+  const error1 = await t.throwsAsync(getInput({}))
+  t.truthy(error1)
 })
