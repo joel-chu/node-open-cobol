ca48ddc90493a3c03c4097934ccfc6511dd99bc5
joel-chu <newbranltd@gmail.com>
2020-10-21 13:26:17 +0800

    update the chainPromises but we might not use it at all

--- index.js
+++ index.js
@@ -3,18 +3,20 @@ const { checkCobc } = require('./lib/check')
 const { prepareInput } = require('./lib/input')
 const { run } = require('./lib/run')
 
+
 /**
  * The main API
  * @public
  * @param {string} strInput file path to the .cbl file or the actual COBOL code
- * @param {object} args extra argument to pass to the cboc
+ * @param {object} options extra argument to pass to the cboc
  * @return {promise}
  */
-function main(strInput, args = {}) {
+function main(strInput, options = {}) {
+
   return checkCobc()
     .then(() => prepareInput(strInput))
     // first run is to tell cobc to compile it to a binary
-    .then(run)
+    .then(run(options))
 }
 
 module.exports = main
