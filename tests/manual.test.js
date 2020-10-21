18472041c680ffda7df98ef1685f4ddc8bcc02e0
joel-chu <newbranltd@gmail.com>
2020-10-21 15:42:17 +0800

    file and argument input all tested

--- tests/manual.test.js
+++ tests/manual.test.js
@@ -9,6 +9,7 @@ const { helloCbl } = require('./fixtures/data')
 const { prepareInput } = require('../lib/input')
 
 const fs = require('fs')
+const path = require('path')
 
 test.skip(`Test the compile method`, async t => {
 
@@ -20,9 +21,23 @@ test.skip(`Test the compile method`, async t => {
 
 })
 
-test(`Test the whole thing end to end`, async t => {
+test.skip(`Test COBOL code input end to end`, async t => {
 
   const result = await main(helloCbl)
 
   t.is(result.trim(), 'Hello world')
+})
+
+test(`Test COBOL file end to end`, async t => {
+  const { resolve, join } = path
+  const pathToFile = resolve(join('tests', 'fixtures', 'args.cbl'))
+
+  console.log('pathToFile', pathToFile)
+
+  const result = await main(pathToFile, { args: ['alice'] })
+
+  console.log(result)
+
+  t.true(result.indexOf('alice') > -1)
+
 })
