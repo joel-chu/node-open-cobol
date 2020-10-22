61ddbce926f8f63c699b0040f617d1763f77c73f
joel-chu <newbranltd@gmail.com>
2020-10-22 20:20:58 +0800

    http test all passed

--- http.js
+++ http.js
@@ -1,22 +1,33 @@
 // The HTTP interface with Koa
 const Koa = require('koa')
+const bodyParser = require('koa-bodyparser')
 const openCobol = require('./index.js')
+const { DEFAULT_PORT, EXT, ARG, OPT } = require('./lib/constants')
+const name = EXT.replace('.', '') // take the dot out
 
 const app = new Koa()
-const port = process.NODE_ENV.PORT || DEFAULT_PORT
+const port = process.env.PORT || DEFAULT_PORT
 
 app.use(bodyParser())
 
 app.use(async ctx => {
-  // the parsed body will store in ctx.request.body
-  // if nothing was parsed, body will be an empty object {}
-  ctx.body = ctx.request.body
+  const json = ctx.request.body
 
-  console.log(ctx.body)
+  ctx.assert(json[name], 403, 'Unsupported input!')
+
+  let params = {[ARG]: json[ARG], [OPT]: json[OPT]}
+
+  const result = await openCobol(json[name], params)
+
+  console.log('RESULT', result)
+
+  ctx.body = { result }
+
 })
 
 if (require.main === module) {
   app.listen(port)
+  console.info(`nodeOpenCobol server started on ${port}`)
 }
-// also export it 
+// also export it
 module.exports = app
--- tests/http.test.js
+++ tests/http.test.js
@@ -1,7 +1,39 @@
 // testing the http interface
 const test = require('ava')
+const request = require('superkoa')
+const fs = require('fs')
+const { helloCbl } = require('./fixtures/data')
+const app = require('../http')
 
 test.before(t => {
+  t.context.app = app
+})
 
+test(`Test the http connection with JSON`, async t => {
+  const res = await request(t.context.app)
+                      .post('/')
+                      .send({cbl: helloCbl})
 
+  console.log('TEST 1', res.body)
+  t.is(200, res.status)
+
+})
+
+test(`Send a wrong parameter name and expect it to fail`, async t => {
+  const res = await request(t.context.app)
+                      .post('/')
+                      .send({wrongName: 'whatever'})
+  console.log('TEST 2', res.body)
+  t.is(403, res.status)
+})
+
+test.only(`Last test with the argument parameter`, async t => {
+  const prog = fs.readFileSync('./tests/fixtures/args.cbl', {encoding: 'utf8'})
+  const res = await request(t.context.app)
+                      .post('/')
+                      .send({cbl: prog, args: ['Alice']})
+
+  console.log('TEST 3', res.body)
+
+  t.is(200, res.status)
 })
