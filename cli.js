28c06ada5988398e2da97d5f67caffa2ee0aea6c
joel-chu <newbranltd@gmail.com>
2020-10-25 22:21:49 +0800

    start the final feature the cli

--- cli.js
+++ cli.js
@@ -1,9 +1,24 @@
-#! /usr/local/bin/node
+#!/usr/bin/env node
 // The command line interface
-const yargs = require('yargs')
+
 const openCobol = require('./index')
 
 // the original node-cobol accept command line input but it really is bs
 // what we want is to able to take all the ARGV then run an interactive
 // session to accept user input before we execute it
 // so this part will be more complex
+
+require('yargs')
+  .scriptName("nodecobc")
+  .usage('$0 <cmd> [args]')
+  .command('hello [name]', 'welcome ter yargs!', (yargs) => {
+    yargs.positional('name', {
+      type: 'string',
+      default: 'Cambi',
+      describe: 'the name to say hello to'
+    })
+  }, function (argv) {
+    console.log('hello', argv.name, 'welcome to yargs!')
+  })
+  .help()
+  .argv
