7334a0fa147d6ad34eafe3358535009a03143e28
joeljiezhu <joelckchu@outlook.com>
2020-10-29 22:14:22 +0800

    implement the get docker images list

--- tests/util.test.js
+++ tests/util.test.js
@@ -2,8 +2,10 @@
 
 const test = require('ava')
 
-
+const { getImages } = require('../lib/docker')
 
 test(`Test the docker related methods`, async t => {
-
+	const result = await getImages()
+	console.info(result)
+	t.truthy(result)
 })
