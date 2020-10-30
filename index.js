908b9ece7929730ffe36584b98474ec1a2bb1c39
joeljiezhu <joelckchu@outlook.com>
2020-10-30 16:12:59 +0800

    setup the main to use the docker options

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 // The main file
-const { checkCobc } = require('./lib/check')
+const { checkCobc, checkDocker } = require('./lib/check')
+const { findImage } = require('./lib/docker')
 const { prepareInput } = require('./lib/input')
 const { run } = require('./lib/run')
+
+const { 
+  DOCKER_NAME,  
+  DEFAULT_DOCKER_IMAGE,
+  CN_SUFFIX 
+} = require('./lib/constants')
+
+/**
+ * The first run to determine if we want to check open-cobol or docker version
+ * @param {object} options the options pass to the init method
+ * @return {promise} 
+ */
+function setupCheck(options) {
+    
+  return options[DOCKER_NAME]
+        ? checkCobc() 
+        : options[DOCKER_NAME] === true // or pass the --cn flag  
+            ? findImage()
+            : findImage([DEFAULT_DOCKER_IMAGE, CN_SUFFIX].join(''))
+}
+
 
 /**
  * The main API
@@ -12,7 +34,7 @@ const { run } = require('./lib/run')
  */
 function main(strInput, options = {}) {
 
-  return checkCobc()
+  return setupCheck(options)
     .then(() => prepareInput(strInput))
     // first run is to tell cobc to compile it to a binary
     .then(results => Reflect.apply(run, null, results.concat(options)))
